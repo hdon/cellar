cc087caae4c502fc5d80e560a8ca1ecc3420cde3
Donny Viszneki <donny@teamspace.blackstreet.strangled.net>
2009-01-17 02:46:52 -0500

    working on stencil buffer ww imp

--- src/ww-gl.cpp
+++ src/ww-gl.cpp
@@ -8,7 +8,8 @@
 #  include <GL/glu.h>
 #endif
 
-Uint32 video_flags = SDL_OPENGL | SDL_RESIZABLE;
+Uint32 video_flags = SDL_OPENGL;
+int width=640, height=480;
 SDL_Surface *screen;
 SDL_Event event;
 
@@ -23,20 +24,65 @@ int main(int argc, char **argv) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) < 0)
         FAIL("SDL_Init()");
 
-    if (SDL_SetVideoMode(640, 480, 0, video_flags) == 0)
-        FAIL("SDL_SetVideoMode()");
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,        0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,        1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,          0);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE,        1);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE,        2);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,      0);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,    0);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,     0);
     SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,    0);
-    glDisable(GL_DEPTH_TEST);
+
+    if (SDL_SetVideoMode(width, height, 0, video_flags) == 0)
+        FAIL("SDL_SetVideoMode()");
+
+    int attr;
+    SDL_GL_GetAttribute(SDL_GL_STENCIL_SIZE, &attr);
+    printf("got %d stencil buffer bits\n", attr);
+    SDL_GL_GetAttribute(SDL_GL_DOUBLEBUFFER, &attr);
+    printf("double buffering %sabled\n", (attr?"en":"dis"));
+
+    /* Acquire a pixel-for-pixel view matrix */
+    glMatrixMode(GL_MODELVIEW);
+    glOrtho(0, width, 0, height, -1, 1);
+
+    /* Enable the feature we need to store our cellular automata */
+    //glEnable(GL_STENCIL_TEST);
+    //glStencilFunc(GL_EQUAL, 
 
     do {
+        /* Paint the color buffer black */
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        /* Paint the color buffer white */
+        glBegin(GL_QUADS);
+        glColor3d(1,1,1);
+        glVertex2d(0,     0);
+        glVertex2d(width, 0);
+        glVertex2d(width, height);
+        glVertex2d(0,     height);
+        glEnd();
+
+        /* Draw some white dots */
+        int x1 = width/4;
+        int y1 = height/4;
+        int x2 = width*3/4;
+        int y2 = height*3/4;
+
+        glBegin(GL_POINTS);
+        glColor3d(0,0,0);
+
+        for (int x=-1; x<=1; x++)
+        for (int y=-1; y<=1; y++) {
+            if (x == y || x == -y) continue;
+            glVertex2d(x1+x, y1+y);
+            glVertex2d(x2+x, y1+y);
+            glVertex2d(x2+x, y2+y);
+            glVertex2d(x1+x, y2+y);
+        }
+        glEnd();
+
+        SDL_GL_SwapBuffers();
+
         SDL_WaitEvent(&event);
     } while (event.type != SDL_QUIT);
 
